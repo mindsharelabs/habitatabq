b90497b19c94083e750c7af2af46b005f135d1dc
James W Johnson <me@thejameswilliam.com>
2024-05-14 20:14:44 -0600

    mobile menu development.

--- template-parts/nav/nav-main.php
+++ template-parts/nav/nav-main.php
@@ -1,3 +1,10 @@
 <?php
-mind_nav('header-menu');
-mind_nav('main-menu');
+
+echo '<nav class="header-menu">';
+    mind_nav('header-menu', array('depth' => 1));
+echo '</nav>';
+
+
+echo '<nav class="main-menu">';
+    mind_nav('main-menu');
+echo '</nav>';
