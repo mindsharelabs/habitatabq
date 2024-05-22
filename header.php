90869800486d68b53e94338315a03b6cf78d180f
James W Johnson <me@thejameswilliam.com>
2024-05-22 13:58:24 -0600

    added favicon.

--- header.php
+++ header.php
@@ -12,7 +12,18 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="<?php bloginfo('description'); ?>">
+        
 
+
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/img/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/img/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/img/favicon-16x16.png">
+        <link rel="manifest" href="<?php echo get_template_directory_uri(); ?>/img/site.webmanifest">
+        <link rel="mask-icon" href="<?php echo get_template_directory_uri(); ?>/img/safari-pinned-tab.svg" color="#00afd7">
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.ico">
+        <meta name="msapplication-TileColor" content="#00afd7">
+        <meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/img/browserconfig.xml">
+        <meta name="theme-color" content="#ffffff">
 
         <?php wp_head(); ?>
     </head>
